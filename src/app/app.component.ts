c99862b0ff3e55db1961e8cea31749e61ff74389
siyavash <siavash.khani200@gmail.com>
2025-03-16 14:06:45 +0330

    feat(app): installed angular/material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,61 @@
-import { Component } from '@angular/core';
-import { Router, RouterModule, RouterOutlet } from '@angular/router';
-import { AuthService } from './auth/auth.service';
-import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  model,
+  signal,
+} from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+interface ITodo {
+  id: number;
+  description: string;
+  done: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, HeaderComponent],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    RouterModule,
+    MatButtonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    FormsModule,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'angular';
-  constructor(private router: Router, public authService: AuthService) {}
+  todoList = signal<ITodo[]>([]);
+
+  description = model('');
+
+  save(): void {
+    const obj: ITodo = {
+      description: this.description(),
+      done: false,
+      id: this.todoList().length + 1,
+    };
+    console.log(obj);
+    this.todoList().push(obj);
+    this.description.set('');
+  }
+
+  checkmarkChanged(index: number): void {
+    this.todoList()[index].done = this.todoList()[index].done;
+    this.todoList.set(this.todoList());
+  }
 }
